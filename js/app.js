0185520e94697933f94bd010943623966b23ec68
Josh Ponelat <josh@ponelat.com>
2015-02-08 09:09:50 +0200

    initial working mustache with partials rig

--- js/app.js
+++ js/app.js
@@ -1,3 +1,57 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+/* global $,Mustache,document */
+
+// -------------- Utilities
+
+// returns array of values found in given hash
+function get_values(col) {
+  return $.map(col, function(value){return value; });
+}
+
+
+
+// --------------- Our code....
+
+// Our mustache partials list, keys map to files found in 'mustache'
+var list_of_partials = {
+  'api-block': $.Deferred()
+};
+
+// AJAX-in each partials
+$.each(list_of_partials, function(name,deferred){
+  $.get('mustache/'+name+'.mustache',function (template) {
+    deferred.resolve({name: name,template: template});
+  })
+  .fail(function(){
+    throw new Error('Could not load in partial: '+ name);
+  });
+});
+
+// Load files in and render
+$.when(
+  $.get('mustache/index.mustache'),
+  $.getJSON('data.json'))
+  .done(function (template,data) {
+
+    // We get resolved objects, when I just want the data...
+    template = template[0];
+    data = data[0];
+
+    // When all partials have been loaded...
+    $.when.apply($, get_values(list_of_partials))
+      .done(function() {
+
+        var partials = {};
+
+        // Add each partial name/template to our hash
+        $.each(arguments, function(i,item){
+          partials[item.name] = item.template;
+        });
+
+        // Render and inject mustaches in all their glory
+        var rendered = Mustache.render( template, data, partials);
+        $(document.body).prepend(rendered);
+
+        // Foundation
+        $(document).foundation();
+    });
+});
