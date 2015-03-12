3f9dae622163774a8c44ecfdf6188ae63e344392
Josh Ponelat <josh@ponelat.com>
2015-03-12 08:22:30 +0200

    add javs.js from swagger

--- code_samples/java.js
+++ code_samples/java.js
@@ -1,211 +1,286 @@
-/* generated for the 'petstore' spec file */
-// Create the parnet object if it doesn't exist...
 window.code = window.code || {};
 
-
-// The schema for each language file is as follows...
-// {
-//   <apiClassName>: {
-
-//     <operationId>: {
-//       <language>: [
-//         {
-//           "header": <string> // THe header displayed, possibly "Method Call", "response",
-//           "syntax": <string> // see https://highlightjs.org/static/demo/ for the supported syntaxes...
-//                            // ...although we would need to make a custom build as the current one only covers the basic syntaxes
-//           "content": <code sample> //escaped for JSON and formated for a <pre> block
-//                                  // ... ie: it needs \n's instead of <br>'s
-//         },
-//         {
-//           "header": <string>
-//           "syntax": <string>
-//           "content": <code sample>
-//         }
-//         ...
-//       ]
-//     },
-
-//     <operationId> : {...}
-
-//   },
-
-//   <apiClassName> { ... }
-
-// }
-
-
-// Here is a live version, that is filled with dummy content
-
 window.code.java = {
-
   "UserApi": {
     "createUser": {
       "java": [
         {
-          "header": "Method Call",
-          "syntax": "java", // see segmentio/highlight for a list of supported syntaxes
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "header": "request",
+          "syntax": "java",
+          "content": "new UserApi().createUser(body)"
         }
       ]
     },
     "createUsersWithArrayInput": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().createUsersWithArrayInput(body)"
         }
       ]
     },
     "createUsersWithListInput": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().createUsersWithListInput(body)"
         }
       ]
     },
     "loginUser": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().loginUser(username, password)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "&quot;aeiou&quot;"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "string"
+        }
+        
       ]
     },
     "logoutUser": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().logoutUser()"
         }
       ]
     },
     "getUserByName": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().getUserByName(username)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "{\n  &quot;id&quot; : 123456789,\n  &quot;lastName&quot; : &quot;aeiou&quot;,\n  &quot;phone&quot; : &quot;aeiou&quot;,\n  &quot;username&quot; : &quot;aeiou&quot;,\n  &quot;email&quot; : &quot;aeiou&quot;,\n  &quot;userStatus&quot; : 123,\n  &quot;firstName&quot; : &quot;aeiou&quot;,\n  &quot;password&quot; : &quot;aeiou&quot;\n}"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "&lt;User&gt;\n  &lt;id&gt;123456&lt;/id&gt;\n  &lt;username&gt;string&lt;/username&gt;\n  &lt;firstName&gt;string&lt;/firstName&gt;\n  &lt;lastName&gt;string&lt;/lastName&gt;\n  &lt;email&gt;string&lt;/email&gt;\n  &lt;password&gt;string&lt;/password&gt;\n  &lt;phone&gt;string&lt;/phone&gt;\n  &lt;userStatus&gt;0&lt;/userStatus&gt;\n&lt;/User&gt;"
+        }
+        
       ]
     },
     "updateUser": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().updateUser(username, body)"
         }
       ]
     },
     "deleteUser": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new UserApi().deleteUser(username)"
         }
       ]
     },
   },
-
   "PetApi": {
     "updatePet": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().updatePet(body)"
         }
       ]
     },
     "addPet": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().addPet(body)"
         }
       ]
     },
     "findPetsByStatus": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().findPetsByStatus(status)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "[ {\n  &quot;tags&quot; : [ {\n    &quot;id&quot; : 123456789,\n    &quot;name&quot; : &quot;aeiou&quot;\n  } ],\n  &quot;id&quot; : 123456789,\n  &quot;category&quot; : {\n    &quot;id&quot; : 123456789,\n    &quot;name&quot; : &quot;aeiou&quot;\n  },\n  &quot;status&quot; : &quot;aeiou&quot;,\n  &quot;name&quot; : &quot;doggie&quot;,\n  &quot;photoUrls&quot; : [ &quot;aeiou&quot; ]\n} ]"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "&lt;Pet&gt;\n  &lt;id&gt;123456&lt;/id&gt;\n  &lt;Category&gt;\n    &lt;id&gt;123456&lt;/id&gt;\n    &lt;name&gt;string&lt;/name&gt;\n  &lt;/Category&gt;\n  &lt;name&gt;doggie&lt;/name&gt;\n  &lt;photoUrls&gt;string&lt;/photoUrls&gt;\n  &lt;Tag&gt;\n    &lt;id&gt;123456&lt;/id&gt;\n    &lt;name&gt;string&lt;/name&gt;\n  &lt;/Tag&gt;\n  &lt;status&gt;string&lt;/status&gt;\n&lt;/Pet&gt;"
+        }
+        
       ]
     },
     "findPetsByTags": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().findPetsByTags(tags)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "[ {\n  &quot;tags&quot; : [ {\n    &quot;id&quot; : 123456789,\n    &quot;name&quot; : &quot;aeiou&quot;\n  } ],\n  &quot;id&quot; : 123456789,\n  &quot;category&quot; : {\n    &quot;id&quot; : 123456789,\n    &quot;name&quot; : &quot;aeiou&quot;\n  },\n  &quot;status&quot; : &quot;aeiou&quot;,\n  &quot;name&quot; : &quot;doggie&quot;,\n  &quot;photoUrls&quot; : [ &quot;aeiou&quot; ]\n} ]"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "&lt;Pet&gt;\n  &lt;id&gt;123456&lt;/id&gt;\n  &lt;Category&gt;\n    &lt;id&gt;123456&lt;/id&gt;\n    &lt;name&gt;string&lt;/name&gt;\n  &lt;/Category&gt;\n  &lt;name&gt;doggie&lt;/name&gt;\n  &lt;photoUrls&gt;string&lt;/photoUrls&gt;\n  &lt;Tag&gt;\n    &lt;id&gt;123456&lt;/id&gt;\n    &lt;name&gt;string&lt;/name&gt;\n  &lt;/Tag&gt;\n  &lt;status&gt;string&lt;/status&gt;\n&lt;/Pet&gt;"
+        }
+        
       ]
     },
     "getPetById": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().getPetById(petId)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "{\n  &quot;tags&quot; : [ {\n    &quot;id&quot; : 123456789,\n    &quot;name&quot; : &quot;aeiou&quot;\n  } ],\n  &quot;id&quot; : 123456789,\n  &quot;category&quot; : {\n    &quot;id&quot; : 123456789,\n    &quot;name&quot; : &quot;aeiou&quot;\n  },\n  &quot;status&quot; : &quot;aeiou&quot;,\n  &quot;name&quot; : &quot;doggie&quot;,\n  &quot;photoUrls&quot; : [ &quot;aeiou&quot; ]\n}"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "&lt;Pet&gt;\n  &lt;id&gt;123456&lt;/id&gt;\n  &lt;Category&gt;\n    &lt;id&gt;123456&lt;/id&gt;\n    &lt;name&gt;string&lt;/name&gt;\n  &lt;/Category&gt;\n  &lt;name&gt;doggie&lt;/name&gt;\n  &lt;photoUrls&gt;string&lt;/photoUrls&gt;\n  &lt;Tag&gt;\n    &lt;id&gt;123456&lt;/id&gt;\n    &lt;name&gt;string&lt;/name&gt;\n  &lt;/Tag&gt;\n  &lt;status&gt;string&lt;/status&gt;\n&lt;/Pet&gt;"
+        }
+        
       ]
     },
     "updatePetWithForm": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().updatePetWithForm(petId, name, status)"
         }
       ]
     },
     "deletePet": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new PetApi().deletePet(api_key, petId)"
+        }
+      ]
+    },
+    "uploadFile": {
+      "java": [
+        {
+          "header": "request",
+          "syntax": "java",
+          "content": "new PetApi().uploadFile(petId, additionalMetadata, file)"
         }
       ]
     },
   },
-
   "StoreApi": {
+    "getInventory": {
+      "java": [
+        {
+          "header": "request",
+          "syntax": "java",
+          "content": "new StoreApi().getInventory()"
+        }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "{\n  &quot;key&quot; : 123\n}"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": ""
+        }
+        
+      ]
+    },
     "placeOrder": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new StoreApi().placeOrder(body)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "{\n  &quot;id&quot; : 123456789,\n  &quot;petId&quot; : 123456789,\n  &quot;complete&quot; : true,\n  &quot;status&quot; : &quot;aeiou&quot;,\n  &quot;quantity&quot; : 123,\n  &quot;shipDate&quot; : &quot;2015-03-11T15:29:25.659+0000&quot;\n}"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "&lt;Order&gt;\n  &lt;id&gt;123456&lt;/id&gt;\n  &lt;petId&gt;123456&lt;/petId&gt;\n  &lt;quantity&gt;0&lt;/quantity&gt;\n  &lt;shipDate&gt;2015-03-11T08:29:25.663Z&lt;/shipDate&gt;\n  &lt;status&gt;string&lt;/status&gt;\n  &lt;complete&gt;true&lt;/complete&gt;\n&lt;/Order&gt;"
+        }
+        
       ]
     },
     "getOrderById": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new StoreApi().getOrderById(orderId)"
         }
+        , {
+          "header": "response: application/json",
+          "syntax": "java",
+          "content": "{\n  &quot;id&quot; : 123456789,\n  &quot;petId&quot; : 123456789,\n  &quot;complete&quot; : true,\n  &quot;status&quot; : &quot;aeiou&quot;,\n  &quot;quantity&quot; : 123,\n  &quot;shipDate&quot; : &quot;2015-03-11T15:29:25.663+0000&quot;\n}"
+        }
+        
+        , {
+          "header": "response: application/xml",
+          "syntax": "java",
+          "content": "&lt;Order&gt;\n  &lt;id&gt;123456&lt;/id&gt;\n  &lt;petId&gt;123456&lt;/petId&gt;\n  &lt;quantity&gt;0&lt;/quantity&gt;\n  &lt;shipDate&gt;2015-03-11T08:29:25.663Z&lt;/shipDate&gt;\n  &lt;status&gt;string&lt;/status&gt;\n  &lt;complete&gt;true&lt;/complete&gt;\n&lt;/Order&gt;"
+        }
+        
       ]
     },
     "deleteOrder": {
       "java": [
         {
-          "header": "Method Call",
+          "header": "request",
           "syntax": "java",
-          "content": "SomeClass aClass = new SomeClass();\naClass.aMethod();"
+          "content": "new StoreApi().deleteOrder(orderId)"
         }
       ]
     },
-  }
+  },
 }
